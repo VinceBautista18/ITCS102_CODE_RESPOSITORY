c55d374444d50a989daf65ecd71649b18de432c6
VinceBautista18 <vincebautista1588@gmail.com>
2026-08-25 12:51:17 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,20 @@
+# + - addition
+# - - subtraction
+# * - multiplication
+# / - division
+# // - floor division (round down)
+# ** - exponentation (exponents
+# % - modulus 
+
+a = 6
+b = 5
+sum = a + b
+diff = a - b
+
+print("The sum of ",a,"plus ",b, "is ",sum)
+print("The sum of ",a,"minus ",b, "is ",diff)
+print("The sum of ",a,"times ",b, "is ",a*b)
+print("The sum of ",a,"divide ",b, "is ",a/b)
+print("The sum of ",a,"divide ",b, "is ",a//b)
+print("The sum of ",a,"exponentation ",b, "is ",a**b)
+print("The sum of ",a,"modulus ",b, "is ",a%b)
