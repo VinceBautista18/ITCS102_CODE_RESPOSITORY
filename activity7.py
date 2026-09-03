0dec344bfb323a004600af1b85aabca0b617e162
VinceBautista18 <vincebautista1588@gmail.com>
2026-09-03 12:24:40 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,6 @@
+x = eval(input("Enter a number ----> ?"))
+
+total = x + 5
+
+#print("Goood Morning," , total)
+print(type(x))
---
+++ activity8.py
@@ -0,0 +1,32 @@
+total = 0
+
+n1 = eval(input("Enter a number ---> "))
+total += n1
+
+n2 = eval(input("Enter a number ---> "))
+total += n2
+
+n3 = eval(input("Enter a number ---> "))
+total += n3
+
+n3 = eval(input("Enter a number ---> "))
+total += n3
+
+n3 = eval(input("Enter a number ---> "))
+total += n3
+
+n3 = eval(input("Enter a number ---> "))
+total += n3
+
+n3 = eval(input("Enter a number ---> "))
+total += n3
+
+n3 = eval(input("Enter a number ---> "))
+total += n3
+
+n3 = eval(input("Enter a number ---> "))
+total -= n3
+
+n3 = eval(input("Enter a number ---> "))
+total -= n3
+print("The sum is ", total)
