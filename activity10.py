1d3e4b91deb8792b86493475c58d44aa40220b0a
VinceBautista18 <vincebautista1588@gmail.com>
2026-09-08 12:30:11 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,11 @@
+a = 15
+
+b = 14
+c = 8
+
+print (a > b or c < a)
+
+print (c < a or c < a and c == b)
+
+print (c < a and c == b)
+print (not(c > a or c < a and c == b))
---
+++ activity11.py
@@ -0,0 +1,11 @@
+username = "bensaw"
+password = "buseng walang kanen"
+
+
+u = input("Input Username -----> ")
+p = input ("Input Password -----> ")
+
+if username == u and p == password :
+	print("ACCESS GRANTED")
+else :
+	print("ACCESS DENIED")
