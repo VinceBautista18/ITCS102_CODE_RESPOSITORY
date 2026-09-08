765687d47c0a0f19d26b6982d86140ae3360432f
VinceBautista18 <vincebautista1588@gmail.com>
2026-09-08 11:52:14 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,20 @@
+x = 10
+y = 5
+
+text1 = "sample"
+text2 = "sample" 
+
+
+print (x > y)
+
+print (x < y)
+
+print (x >= y)
+
+print (x <= y)
+
+print (x == y)
+
+print (text1 == text2)
+
+print (text1 != text2)
