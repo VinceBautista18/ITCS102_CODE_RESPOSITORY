8e5f4b1d838cd943d707de301afc43a4b2c31123
VinceBautista18 <vincebautista1588@gmail.com>
2026-09-10 21:10:58 +0800

    Code challenge 3
    Refactored shipping cost calculation and input handling.

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,45 +1,60 @@
-name = input("Input Sender name ----> ")
+print("    TINDAHAN NI ALING NENA ")
+print("|-----------------------------|")
 
-type = input("Input Type of Item ----> ")
+name = input("What is your name? --> ")
+item = input("What is your order? --> ")
 
-is_Fragile = bool(input("Is the Product Fragile?( Enter \"yes\" if yes, press Enter if no) -----> "))
-
-weight = float(input("Weight of the product? (in kg) ----> "))
-
-distance = float(input("Distance? (in km) ----->"))
-
-is_Express = bool(input("Is it Expres? (Enter \"yes\" if yes, press Enter if no) ----> "))
-
-is_International = bool(input("Is it International? (Enter \"yes\" if yes, press Enter if no) ---> "))
-
-base_cost = (weight * 2.50) + (distance * 0.15)
-
-if weight <= 2 and distance  <= 100 and is_Express == False and is_International == False :
-	print("FREE SHIPPING")
-	Total = 0
-
-elif is_International == True and is_Express == True :
-	print("Package is Express or Heavy International is applied")
-	Total = (base_cost * 1.2) + 50
-
-elif is_Express == True or (is_International == True and weight > 20):
-	print("Package is Express or Heavy International is applied") 
-	Total = (base_cost * 1.2) + 25
-
-elif weight > 30 or distance > 1000 :
-	print("Oversized is applied")
-	Total = base_cost + 30
-
+fragile_input = input("Is it fragile? (yes/no) --> ")
+if fragile_input == "yes":
+  is_fragile = True
 else:
-	Total = base_cost
-	print("Standard rate is applied")
-	
+  is_fragile = False
 
-print ("Hi, ",name, ", how are you doing today ?")
+weight = float(input("How heavy is it in kg? --> "))
+distance = float(input("How far is the destination in km? --> "))
 
-print ("Your ",type, ",order is confirmed ")
+express_input = input("Is it in a hurry (express)? (yes/no) --> ")
+if express_input == "yes":
+  is_express = True
+else:
+  is_express = False
 
-print("Total Output : PHP ", Total)
+international_input = input("Is it international? (yes/no) --> ")
+if international_input == "yes":
+  is_international = True
+else:
+  is_international = False
 
+base_cost = (weight * 2.5) + (distance * 0.15)
 
+if (
+    distance <= 100
+    and weight <= 2
+    and not is_express
+    and not is_international
+):
+  total = 0
+elif is_international and is_express:
+  total = (base_cost * 1.4) + 50
+elif weight > 20 and (is_international or is_express):
+  total = (base_cost * 1.2) + 25
+elif distance > 1000 or weight > 30:
+  total = base_cost + 30
+else:
+  total = base_cost
 
+if total == 0:
+  shipping_fee = 0
+else:
+  shipping_fee = total - base_cost
+
+print("Sender:", name)
+print("Order:", item)
+print("Fragile:", is_fragile)
+print("Weight:", weight, "kg")
+print("Distance:", distance, "km")
+print("Express:", is_express)
+print("International:", is_international)
+print("Base Cost: PHP", base_cost)
+print("Shipping Fee: PHP", shipping_fee)
+print("Total: PHP", total)
